016cb1cbe47afff95059a7ae993a457fd801db82
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-07-09 01:53:46 -0600

    examen

--- app/Http/Controllers/QueriesController.php
+++ app/Http/Controllers/QueriesController.php
@@ -20,7 +20,7 @@ class QueriesController extends Controller
         $query3 = employee::pluck('department_id');
 
         //Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado, eliminando los identificadores que aparecen repetidos
-        $query4 = employee::select('department_id')->distinct()->pluck('department_id');
+        $query4 = employee::select('department_id')->pluck('department_id');
 
         //Lista el nombre y apellidos de los empleados en una única columna
         $query5 = employee::selectRaw("CONCAT_WS(' ', first_name, last_name1, last_name2) as full_name")->get();
@@ -32,13 +32,13 @@ class QueriesController extends Controller
         $query7 = department::selectRaw('name, (budget - expense) as presupuesto_actual')->orderBy('name', 'asc')->get();
 
         //Lista el nombre de todos los departamentos ordenados de forma desscendente
-        $query8 = department::orderBy('name', 'desc')->distinct()->pluck('name');
+        $query8 = department::orderBy('name', 'desc')->pluck('name');
 
         //Lista los apellidos y el nombre de todos los empleados, ordenados de forma alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su nombre.
         $query9 = Employee::selectRaw("CONCAT_WS(' ', last_name1, COALESCE(last_name2, ''), first_name) as full_name")->orderBy('full_name', 'asc')->distinct()->pluck('full_name');
 
         //Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen mayor presupuesto
-        $query10 = department::selectRaw('name, (budget - expense) as presupuesto_actual')->distinct()->orderBy('presupuesto_actual', 'desc')->take(3)->get();
+        $query10 = department::selectRaw('name, (budget - expense) as presupuesto_actual')->orderBy('presupuesto_actual', 'desc')->take(3)->get();
 
         return view('index', compact('query1', 'query2', 'query3', 'query4', 'query5', 'query6', 'query7', 'query8',  'query9', 'query10'));
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,17 +17,19 @@ class DatabaseSeeder extends Seeder
         Department::create(['name' => 'Projects', 'budget' => 375000, 'expense' => 380000]);
         Department::create(['name' => 'Advertising', 'budget' => 0, 'expense' => 0]);
 
-        Employee::create(['nif' => '32481596J', 'first_name' => 'Aarón', 'last_name1' => 'Rivero', 'last_name2' => 'Gómez', 'department_id' => 1]);
-        Employee::create(['nif' => 'YS575638Z', 'first_name' => 'Adela', 'last_name1' => 'Salas', 'last_name2' => 'Díaz', 'department_id' => 1]);
-        Employee::create(['nif' => 'R9705618A', 'first_name' => 'Adrián', 'last_name1' => 'Delgado', 'last_name2' => 'Calvo', 'department_id' => 2]);
-        Employee::create(['nif' => '11708236X', 'first_name' => 'Macías', 'last_name1' => 'Loyola', 'last_name2' => 'Méndez', 'department_id' => 4]);
-        Employee::create(['nif' => '33882980M', 'first_name' => 'Sara', 'last_name1' => 'Piña', 'last_name2' => 'Hernández', 'department_id' => 3]);
-        Employee::create(['nif' => '80756690X', 'first_name' => 'Pilar', 'last_name1' => 'Ruiz', 'last_name2' => null, 'department_id' => 2]);
-        Employee::create(['nif' => '71614513Z', 'first_name' => 'Pepe', 'last_name1' => 'Ruiz', 'last_name2' => 'Santana', 'department_id' => 3]);
+        Employee::create(['nif' => '32481596F', 'first_name' => 'Aarón', 'last_name1' => 'Rivero', 'last_name2' => 'Gómez', 'department_id' => 1]);
+        Employee::create(['nif' => 'Y5575632D', 'first_name' => 'Adela', 'last_name1' => 'Salas', 'last_name2' => 'Díaz', 'department_id' => 2]);
+        Employee::create(['nif' => 'R6970642B', 'first_name' => 'Adolfo', 'last_name1' => 'Rubio', 'last_name2' => 'Flores', 'department_id' => 3]);
+        Employee::create(['nif' => '77705545E', 'first_name' => 'Adrián', 'last_name1' => 'Suárez', 'last_name2' => null, 'department_id' => 4]);
+        Employee::create(['nif' => '17087203C', 'first_name' => 'Marcos', 'last_name1' => 'Loyola', 'last_name2' => 'Méndez', 'department_id' => 5]);
+        Employee::create(['nif' => '38382980M', 'first_name' => 'María', 'last_name1' => 'Santana', 'last_name2' => 'Moreno', 'department_id' => 1]);
+        Employee::create(['nif' => '80576669X', 'first_name' => 'Pilar', 'last_name1' => 'Ruiz', 'last_name2' => null, 'department_id' => 2]);
+        Employee::create(['nif' => '71651431Z', 'first_name' => 'Pepe', 'last_name1' => 'Ruiz', 'last_name2' => 'Santana', 'department_id' => 3]);
         Employee::create(['nif' => '56399183D', 'first_name' => 'Juan', 'last_name1' => 'Gómez', 'last_name2' => 'López', 'department_id' => 2]);
         Employee::create(['nif' => '46384486H', 'first_name' => 'Diego', 'last_name1' => 'Flores', 'last_name2' => 'Salas', 'department_id' => 5]);
-        Employee::create(['nif' => '6789283A', 'first_name' => 'Marta', 'last_name1' => 'Herrera', 'last_name2' => 'Gil', 'department_id' => null]);
-        Employee::create(['nif' => '41284386R', 'first_name' => 'Irene', 'last_name1' => 'Salas', 'last_name2' => 'Flores', 'department_id' => null]);
-        Employee::create(['nif' => '82365162B', 'first_name' => 'Juan Antonio', 'last_name1' => 'Sáez', 'last_name2' => 'Guerrero', 'department_id' => null]);
+        Employee::create(['nif' => '67389283A', 'first_name' => 'Marta', 'last_name1' => 'Herrera', 'last_name2' => 'Gil', 'department_id' => 1]);
+        Employee::create(['nif' => '41234836R', 'first_name' => 'Irene', 'last_name1' => 'Salas', 'last_name2' => 'Flores', 'department_id' => null]);
+        Employee::create(['nif' => '82635162B', 'first_name' => 'Juan Antonio', 'last_name1' => 'Sáez', 'last_name2' => 'Guerrero', 'department_id' => null]);
+
     }
 }

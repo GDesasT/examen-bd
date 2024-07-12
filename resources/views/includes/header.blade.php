fbd956df3b57d6addd8a73ff355ec0607ff6263d
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-07-12 00:50:52 -0600

    examen

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Empleados y Departamentos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    @include('includes.navbar')
+
+    <div class="container mt-4">
+        <h2>Listado de Empleados y Departamentos</h2>
+</body>
+</html>
